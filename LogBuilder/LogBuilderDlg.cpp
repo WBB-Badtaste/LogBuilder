1c6d8e2db24c716973ee08cf9c93733b2c0ef8e9
WBB-Badtaste <sos901012@gmail.com>
2014-10-24 10:23:41 +0800

    Details Modify

--- LogBuilder/LogBuilderDlg.cpp
+++ LogBuilder/LogBuilderDlg.cpp
@@ -85,10 +85,11 @@ BOOL CLogBuilderDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 设置大图标
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
-	ShowWindow(SW_MINIMIZE);
+	ShowWindow(SW_SHOWNORMAL);
 
 	// TODO: 在此添加额外的初始化代码
-
+	CenterWindow();
+	
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
 
@@ -119,6 +120,8 @@ void CLogBuilderDlg::OnPaint()
 	{
 		CDialogEx::OnPaint();
 	}
+//用户代码
+	GetDlgItem(IDC_ITEM1)->SetFocus();
 }
 
 //当用户拖动最小化窗口时系统调用此函数取得光标
@@ -134,65 +137,33 @@ void CLogBuilderDlg::OnSetfocusItem1()
 {
 	// TODO: 在此添加控件通知处理程序代码
 	
-	CTime t=CTime::GetCurrentTime ();
-	if(m_item1=="")
-	{
-		UpdateData(TRUE);
-		m_start1=t.Format(_T("%H:%M"));
-		UpdateData(FALSE);
-	}
 }
 
 
 void CLogBuilderDlg::OnSetfocusItem2()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	CTime t=CTime::GetCurrentTime ();
-	if(m_item2=="")
-	{
-		UpdateData(TRUE);
-		m_start2=t.Format(_T("%H:%M"));
-		UpdateData(FALSE);
-	}
+
 }
 
 
 void CLogBuilderDlg::OnSetfocusItem3()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	CTime t=CTime::GetCurrentTime ();
-	if(m_item3=="")
-	{
-		UpdateData(TRUE);
-		m_start3=t.Format(_T("%H:%M"));
-		UpdateData(FALSE);
-	}
+
 }
 
 
 void CLogBuilderDlg::OnSetfocusItem4()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	CTime t=CTime::GetCurrentTime ();
-	if(m_item4=="")
-	{
-		UpdateData(TRUE);
-		m_start4=t.Format(_T("%H:%M"));
-		UpdateData(FALSE);
-	}
+
 }
 
 
 void CLogBuilderDlg::OnSetfocusItem5()
 {
 	// TODO: 在此添加控件通知处理程序代码
-	CTime t=CTime::GetCurrentTime ();
-	if(m_item5=="")
-	{
-		UpdateData(TRUE);
-		m_start5=t.Format(_T("%H:%M"));
-		UpdateData(FALSE);
-	}
 }
 
 
@@ -200,145 +171,221 @@ BOOL CLogBuilderDlg::PreTranslateMessage(MSG*   pMsg)
 {  
 //	if(pMsg->message==WM_KEYDOWN   &&   pMsg->wParam==VK_ESCAPE)     return   TRUE;  
 	
-	if(pMsg->message==WM_KEYDOWN   &&   pMsg->wParam==VK_RETURN)  
+	if(pMsg->message==WM_KEYDOWN)
 	{
-		UpdateData(TRUE);
-		CTime t(0);
-		CEdit *p=0;
-		int selStart=0,selEnd=0;
-		CString buffer("");
-		switch( GetFocus()->GetDlgCtrlID() )
+		if(pMsg->wParam==VK_RETURN)//输入回车
 		{
-		case IDC_ITEM1:
-			if(m_item1!="")
+			UpdateData(TRUE);
+			CTime t(0);
+			CEdit *p=0;
+			int selStart=0,selEnd=0;
+			CString buffer("");
+			switch( GetFocus()->GetDlgCtrlID() )
 			{
-				p=(CEdit*)GetDlgItem(IDC_ITEM1);				
-				p->GetSel(selStart,selEnd);
-				if(selEnd-selStart==p->LineLength())
+			case IDC_ITEM1:
+				if(m_item1!="")
+				{
+					p=(CEdit*)GetDlgItem(IDC_ITEM1);			
+					p->GetSel(selStart,selEnd);
+					if(selEnd-selStart==p->LineLength())
+					{
+						buffer=m_start1+_T("~")+m_end1+_T(" ")+m_item1+_T("\n");
+						WriteLog(buffer);
+						m_start1=m_end1=m_item1=_T("");
+					}
+					else
+					{
+						p->SetSel(0,p->LineLength());
+						t=CTime::GetCurrentTime ();
+						m_end1=t.Format(_T("%H:%M"));
+					}
+				}
+				break;
+			case IDC_ITEM2:
+				if(m_item2!="")
+				{
+					p=(CEdit*)GetDlgItem(IDC_ITEM1);				
+					p->GetSel(selStart,selEnd);
+					if(selEnd-selStart==p->LineLength())
+					{
+						buffer=m_start2+_T("~")+m_end2+_T(" ")+m_item2+_T("\n");
+						WriteLog(buffer);
+						m_start2=m_end2=m_item2=_T("");
+					}
+					else
+					{
+						p->SetSel(0,p->LineLength());
+						t=CTime::GetCurrentTime ();
+						m_end2=t.Format(_T("%H:%M"));
+					}
+				}
+				break;
+			case IDC_ITEM3:
+				if(m_item3!="")
+				{
+					p=(CEdit*)GetDlgItem(IDC_ITEM1);				
+					p->GetSel(selStart,selEnd);
+					if(selEnd-selStart==p->LineLength())
+					{
+						buffer=m_start3+_T("~")+m_end3+_T(" ")+m_item3+_T("\n");
+						WriteLog(buffer);
+						m_start3=m_end3=m_item3=_T("");
+					}
+					else
+					{
+						p->SetSel(0,p->LineLength());
+						t=CTime::GetCurrentTime ();
+						m_end3=t.Format(_T("%H:%M"));
+					}
+				}
+				break;
+			case IDC_ITEM4:
+				if(m_item4!="")
+				{
+					p=(CEdit*)GetDlgItem(IDC_ITEM1);				
+					p->GetSel(selStart,selEnd);
+					if(selEnd-selStart==p->LineLength())
+					{
+						buffer=m_start4+_T("~")+m_end4+_T(" ")+m_item4+_T("\n");
+						WriteLog(buffer);
+						m_start4=m_end4=m_item4=_T("");
+					}
+					else
+					{
+						p->SetSel(0,p->LineLength());
+						t=CTime::GetCurrentTime ();
+						m_end4=t.Format(_T("%H:%M"));
+					}
+				}
+				break;
+			case IDC_ITEM5:
+				if(m_item5!="")
+				{
+					p=(CEdit*)GetDlgItem(IDC_ITEM1);				
+					p->GetSel(selStart,selEnd);
+					if(selEnd-selStart==p->LineLength())
+					{
+						buffer=m_start5+_T("~")+m_end5+_T(" ")+m_item5+_T("\n");
+						WriteLog(buffer);
+						m_start5=m_end5=m_item5=_T("");
+					}
+					else
+					{
+						p->SetSel(0,p->LineLength());
+						t=CTime::GetCurrentTime ();
+						m_end5=t.Format(_T("%H:%M"));
+					}
+				}
+				break;
+			case IDC_REMARK:
+			case IDC_PROJECTNAME:
+				if(m_remark!=""&&m_projectName!="")
+				{
+					p=(CEdit*)GetDlgItem(IDC_REMARK);				
+					buffer=m_remark+_T("\n");
+					WriteMemo(buffer);
+					m_remark=_T("");
+				}
+				break;
+			case IDC_START1:
+			case IDC_END1:
+				if(m_item1!="")
 				{
 					buffer=m_start1+_T("~")+m_end1+_T(" ")+m_item1+_T("\n");
 					WriteLog(buffer);
 					m_start1=m_end1=m_item1=_T("");
 				}
-				else
-				{
-					p->SetSel(0,p->LineLength());
-					t=CTime::GetCurrentTime ();
-					m_end1=t.Format(_T("%H:%M"));
-				}
-			}
-			break;
-		case IDC_ITEM2:
-			if(m_item2!="")
-			{
-				p=(CEdit*)GetDlgItem(IDC_ITEM1);				
-				p->GetSel(selStart,selEnd);
-				if(selEnd-selStart==p->LineLength())
+				break;
+			case IDC_START2:
+			case IDC_END2:
+				if(m_item2!="")
 				{
 					buffer=m_start2+_T("~")+m_end2+_T(" ")+m_item2+_T("\n");
 					WriteLog(buffer);
 					m_start2=m_end2=m_item2=_T("");
 				}
-				else
-				{
-					p->SetSel(0,p->LineLength());
-					t=CTime::GetCurrentTime ();
-					m_end2=t.Format(_T("%H:%M"));
-				}
-			}
-			break;
-		case IDC_ITEM3:
-			if(m_item3!="")
-			{
-				p=(CEdit*)GetDlgItem(IDC_ITEM1);				
-				p->GetSel(selStart,selEnd);
-				if(selEnd-selStart==p->LineLength())
+				break;
+			case IDC_START3:
+			case IDC_END3:
+				if(m_item3!="")
 				{
 					buffer=m_start3+_T("~")+m_end3+_T(" ")+m_item3+_T("\n");
 					WriteLog(buffer);
 					m_start3=m_end3=m_item3=_T("");
 				}
-				else
-				{
-					p->SetSel(0,p->LineLength());
-					t=CTime::GetCurrentTime ();
-					m_end3=t.Format(_T("%H:%M"));
-				}
-			}
-			break;
-		case IDC_ITEM4:
-			if(m_item4!="")
-			{
-				p=(CEdit*)GetDlgItem(IDC_ITEM1);				
-				p->GetSel(selStart,selEnd);
-				if(selEnd-selStart==p->LineLength())
+				break;
+			case IDC_START4:
+			case IDC_END4:
+				if(m_item4!="")
 				{
 					buffer=m_start4+_T("~")+m_end4+_T(" ")+m_item4+_T("\n");
 					WriteLog(buffer);
 					m_start4=m_end4=m_item4=_T("");
 				}
-				else
-				{
-					p->SetSel(0,p->LineLength());
-					t=CTime::GetCurrentTime ();
-					m_end4=t.Format(_T("%H:%M"));
-				}
-			}
-			break;
-		case IDC_ITEM5:
-			if(m_item5!="")
-			{
-				p=(CEdit*)GetDlgItem(IDC_ITEM1);				
-				p->GetSel(selStart,selEnd);
-				if(selEnd-selStart==p->LineLength())
+				break;
+			case IDC_START5:
+			case IDC_END5:
+				if(m_item5!="")
 				{
 					buffer=m_start5+_T("~")+m_end5+_T(" ")+m_item5+_T("\n");
 					WriteLog(buffer);
 					m_start5=m_end5=m_item5=_T("");
 				}
-				else
-				{
-					p->SetSel(0,p->LineLength());
-					t=CTime::GetCurrentTime ();
-					m_end5=t.Format(_T("%H:%M"));
-				}
+				break;
+			default:
+				break;
 			}
-			break;
-		case IDC_REMARK:
-			if(m_remark!=""&&m_projectName!="")
+			UpdateData(FALSE);
+			return TRUE;
+		}
+		switch(GetFocus()->GetDlgCtrlID())//输入开始，显示时间
+		{
+		case IDC_ITEM1:
+			if(m_item1=="")
 			{
-				p=(CEdit*)GetDlgItem(IDC_REMARK);				
-				buffer=m_remark+_T("\n");
-				WriteMemo(buffer);
-				m_remark=_T("");
-				m_projectName=m_remark=_T("");
+				CTime t=CTime::GetCurrentTime ();
+				UpdateData(TRUE);
+				m_start1=t.Format(_T("%H:%M"));
+				UpdateData(FALSE);
 			}
 			break;
-		case IDC_PROJECTNAME:
-			if(m_remark!=""&&m_projectName!="")
+		case IDC_ITEM2:
+			if(m_item2=="")
 			{
-				p=(CEdit*)GetDlgItem(IDC_REMARK);				
-				buffer=m_remark+_T("\n");
-				WriteMemo(buffer);
-				m_remark=_T("");
-				m_projectName=m_remark=_T("");
+				CTime t=CTime::GetCurrentTime ();
+				UpdateData(TRUE);
+				m_start2=t.Format(_T("%H:%M"));
+				UpdateData(FALSE);
 			}
 			break;
-		case IDC_END1:
+		case IDC_ITEM3:
+			if(m_item3=="")
+			{
+				CTime t=CTime::GetCurrentTime ();
+				UpdateData(TRUE);
+				m_start3=t.Format(_T("%H:%M"));
+				UpdateData(FALSE);
+			}
 			break;
-		case IDC_END2:
+		case IDC_ITEM4:
+			if(m_item4=="")
+			{
+				CTime t=CTime::GetCurrentTime ();
+				UpdateData(TRUE);
+				m_start4=t.Format(_T("%H:%M"));
+				UpdateData(FALSE);
+			}
 			break;
-		case IDC_END3:
-			break;
-		case IDC_END4:
-			break;
-		case IDC_END5:
-			break;
-		default:
+		case IDC_ITEM5:
+			if(m_item5=="")
+			{
+				CTime t=CTime::GetCurrentTime ();
+				UpdateData(TRUE);
+				m_start5=t.Format(_T("%H:%M"));
+				UpdateData(FALSE);
+			}
 			break;
 		}
-		UpdateData(FALSE);
-		return TRUE;
 	}
 	return CDialog::PreTranslateMessage(pMsg);  
 }
@@ -356,7 +403,7 @@ BOOL CLogBuilderDlg::WriteLog(const CString &str)
 
 BOOL CLogBuilderDlg::WriteMemo(const CString &str)
 {
-	m_file.Open(_T("E:\\资料文档\\工程备忘\\")+m_projectName+_T("memo.doc"),CFile::modeCreate|CFile::modeNoTruncate|CFile::modeWrite);
+	m_file.Open(_T("E:\\资料文档\\工程备忘\\")+m_projectName+_T("_Memo.doc"),CFile::modeCreate|CFile::modeNoTruncate|CFile::modeWrite);
 	m_file.SeekToEnd(); 
 	m_file.Write((LPCTSTR)str, str.GetLength());
 	m_file.Close();
